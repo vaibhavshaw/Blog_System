16b778799214366854c091b8ccf9e798b772f882
Vaibhav Shaw <vaibhavshaw028@gmail.com>
2019-04-15 02:42:25 +0530

    Passport Authentication

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // const db = require("./config/keys").mongoURI;
 
@@ -18,10 +23,13 @@ mongoose
   .catch(err => {
     console.log(err);
   });
-
+/*
 app.get("/", (req, res) => {
   res.send("Hello, yeah");
 });
+*/
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
